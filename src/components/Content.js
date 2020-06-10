c93abd9af3a1a92cdfc47679aa8567409f9ad9a0
rori4 <rangelstoilov@gmail.com>
2020-06-10 14:11:09 +0300

    ⛪ ✝ resurrect broken static site ✝

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,17 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 export const HTMLContent = ({ content, className }) => (
-//   <div className={className} dangerouslySetInnerHTML={{ __html: content }} />
-// )
+  <div className={className} dangerouslySetInnerHTML={{ __html: content }} />
+)
 
-// const Content = ({ content, className }) => (
-//   <div className={className}>{content}</div>
-// )
+const Content = ({ content, className }) => (
+  <div className={className}>{content}</div>
+)
 
-// Content.propTypes = {
-//   content: PropTypes.node,
-//   className: PropTypes.string,
-// }
+Content.propTypes = {
+  content: PropTypes.node,
+  className: PropTypes.string,
+}
 
 HTMLContent.propTypes = Content.propTypes
 
